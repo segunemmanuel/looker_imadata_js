0af9b03f5a859415c10426393cd7b6975b30fb80
Shegz <segunemmanuel46@gmail.com>
2023-08-03 16:44:48 +0100

    first commit

---
+++ imadata.js
@@ -0,0 +1,12 @@
+var iframeUrl = "https://lookerstudio.google.com/embed/reporting/9ab7bf86-48b7-400d-8d1f-b48bbdd0092b/page/p_u5yiyboe3c";
+
+var containerElement = document.getElementsByClassName("issuu-embed-container");
+
+var iframeElement = document.createElement("iframe");
+
+iframeElement.src = iframeUrl;
+
+iframe.frameBorder = "0";
+iframe.allowFullscreen = true;
+
+containerElement.appendChild(iframeElement);
